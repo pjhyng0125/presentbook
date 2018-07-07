75e045ffbb2d091d661b78cc248cd28c580eec06
Seung Hwan <suhseunghwan2@gmail.com>
2018-07-07 15:02:56 +0900

    북 수정_15:02

--- pb/src/playdata/pb/view/BookView.java
+++ pb/src/playdata/pb/view/BookView.java
@@ -6,31 +6,31 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class BookView extends JFrame{
-	JLabel lblNewLabel ;
-	JButton btnNewButton ;
-	JButton btnNewButton_1 ;
-	JButton button;
-	JPanel panel ;
+	JLabel la_book ;
+	JButton bt_back ;
+	JButton bt_next ;
+	JButton bt_prev;
+	JPanel panel_book ;
 	public BookView() {
 		getContentPane().setLayout(null);
 		
-		lblNewLabel	= new JLabel("책 제목");
-		btnNewButton = new JButton("메인페이지");
-		JPanel panel = new JPanel();
-		btnNewButton_1 = new JButton("이전");
-		JButton button = new JButton("다음");
+		la_book	= new JLabel("책 제목");
+		bt_back = new JButton("메인페이지");
+		panel_book = new JPanel();
+		bt_prev = new JButton("이전");
+		 bt_next = new JButton("다음");
 		
-		panel.setBounds(133, 54, 1164, 615);
-		btnNewButton.setBounds(14, 13, 105, 27);
-		btnNewButton_1.setBounds(14, 331, 105, 68);
-		lblNewLabel.setBounds(417, 12, 640, 28);
-		button.setBounds(1301, 331, 105, 68);
+		 panel_book.setBounds(133, 54, 1164, 615);
+		 bt_back.setBounds(14, 13, 105, 27);
+		 bt_prev.setBounds(14, 331, 105, 68);
+		la_book.setBounds(417, 12, 640, 28);
+		bt_next.setBounds(1301, 331, 105, 68);
 		
-		getContentPane().add(lblNewLabel);
-		getContentPane().add(btnNewButton);
-		getContentPane().add(panel);
-		getContentPane().add(btnNewButton_1);
-		getContentPane().add(button);
+		getContentPane().add(la_book);
+		getContentPane().add(bt_back);
+		getContentPane().add(panel_book);
+		getContentPane().add(bt_prev);
+		getContentPane().add(bt_next);
 		
 		setVisible(true);
 	}
