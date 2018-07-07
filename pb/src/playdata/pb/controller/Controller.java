cfbb5c62cab2d416512fdc31595375729585cbff
park <pjhyng0125@naver.com>
2018-07-07 10:43:14 +0900

    진형
    update ojdbc6

--- pb/src/playdata/pb/controller/Controller.java
+++ pb/src/playdata/pb/controller/Controller.java
@@ -1,5 +1,39 @@
 package playdata.pb.controller;
 
-public class Controller {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import playdata.pb.view.JoinView;
+import playdata.pb.view.LoginView;
+import playdata.pb.view.PassUpdateView;
+
+public class Controller implements ActionListener {
+	LoginView v_login;
+	JoinView v_join;
+	PassUpdateView v_passup;
+	
+	public Controller() {
+		v_login = new LoginView();
+		v_join = new JoinView();
+		v_passup = new PassUpdateView();
+		
+		eventup();
+	}
+	
+	public void eventup() {
+		v_login.bt_join.addActionListener(this);
+	}
+	
+	public static void main(String[] args) {
+		new Controller();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object ob = e.getSource();
+		if(ob == v_login.bt_join) {
+			v_login.setVisible(false);
+			v_join.setVisible(true);
+		}
+	}
 }
--- pb/src/playdata/pb/view/BookGenreView.java
+++ pb/src/playdata/pb/view/BookGenreView.java
@@ -1,5 +1,46 @@
 package playdata.pb.view;
 
-public class BookGenreView {
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
 
+public class BookGenreView extends JFrame {
+	public JLabel la_msg;
+	public JRadioButton rb_1, rb_2, rb_3;
+	public ButtonGroup bg;
+	public JButton bt_select;
+	public BookGenreView() {
+		setTitle("BookGenreView");
+		setLayout(null);
+//new
+		la_msg = new JLabel("장르를 선택하세요");
+		rb_1 = new JRadioButton("소설");
+		rb_2 = new JRadioButton("수필");
+		rb_3 = new JRadioButton("스릴러");
+		bg = new ButtonGroup();
+		bt_select = new JButton("장르 선택");
+		bg.add(rb_1);
+		bg.add(rb_2);
+		bg.add(rb_3);
+//setBounds
+		la_msg.setBounds(60, 30, 120, 30);
+		rb_1.setBounds(20, 100 , 80, 30);
+		rb_2.setBounds(120, 100 , 80, 30);
+		rb_3.setBounds(200, 100 , 80, 30);
+		bt_select.setBounds(60, 200, 90, 30);
+// frame
+		add(la_msg);
+		add(rb_1);
+		add(rb_2);
+		add(rb_3);
+		add(bt_select);
+		
+		setSize(300, 300);
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		new BookGenreView();
+	}
 }
